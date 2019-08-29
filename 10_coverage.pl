5b61da47a7e8dbe6fa5f94469c0c72159c6f9df1
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2019-08-29 20:07:46 +0200

    10_coverage.pl initial commit, along with the associated datadir

---
+++ 10_coverage.pl
@@ -0,0 +1,178 @@
+#!/usr/bin/perl
+
+# 29/08/2019
+# NTM
+
+# Takes 2 args: $exonsOfInterest and $gvcf
+# $exonsOfInterest is a tsv holding exon coordinates of genes of interest;
+# $gvcf is a GVCF file, must be tabix-indexed.
+# Print to stdout a tsv with one line per exon, for each exon we report
+# the percentage of its bases (going to +-10 into neighboring introns)
+# that are covered at least 10x or 20x.
+
+
+use strict;
+use warnings;
+
+
+(@ARGV == 2) || die "needs 2 args: a tsv with exons of interest and a GVCF file\n";
+my ($exonsOfInterest, $gvcf) = @ARGV;
+(-f $exonsOfInterest) ||
+    die "exonsOfInterest $exonsOfInterest doesn't exist or isn't a file\n";
+(-f $gvcf) ||
+    die "GVCF $gvcf doesn't exist or isn't a file\n";
+(-f "$gvcf.tbi") ||
+    die "GVCF $gvcf exists but can't find its index file $gvcf.tbi, did you tabix-index the gvcf?\n";
+
+
+open(GENES,$exonsOfInterest) || 
+    die "cannot open exonsOfInterest $exonsOfInterest\n";
+
+# skip header, 2 lines but check second one
+<GENES>;
+my $header = <GENES>;
+chomp($header);
+($header eq "#hg38.knownGene.name\thg38.knownGene.chrom\thg38.knownGene.exonStarts\thg38.knownGene.exonEnds\thg38.kgXref.geneSymbol") ||
+    die "header of exonsOfInterest $exonsOfInterest isn't as expected, remake the file correctly or fix the code\n";
+
+# print my own header
+print "Gene\tTranscript\tExon\tExon length (+20)\tPercentage covered >= 20x\tPercentage covered >= 10x\n";
+
+while (my $line = <GENES>) {
+    chomp($line);
+    my @fields = split(/\t/,$line);
+    (@fields == 5) || 
+	die "wrong number of fields in line:\n$line\n";
+    my ($transcript,$chr,$starts,$ends,$gene) = @fields;
+
+    my @starts = split(/,/,$starts);
+    my $numExons = @starts;
+    my @ends = split(/,/,$ends);
+    ($numExons == @ends) || 
+	die "mismatch between numbers of starts and ends in line:\n$line\n";
+
+    # we will print one line per exon, but also a final line for the whole gene/transcript
+    my $lengthGene = 0;
+    # we will count the number of bases in this gene whose coverage $cov is:
+    # $cov >= 20x, 10x <= $cov < 20x, or $cov < 10x respectively
+    my ($bases20Gene,$bases10Gene,$bases0Gene) = (0,0,0);
+
+    foreach my $i (0..$#starts) {
+	# for Exon use eg 3\12 (so excel doesn't corrupt my file)
+	my $toPrint = "$gene\t$transcript\t";
+	$toPrint .= $i+1;
+	$toPrint .= "\\$numExons\t";
+	# end-start+1 is the exon length, add 10 bases on each side
+	my $length = $ends[$i] - $starts[$i] + 21;
+	$toPrint .= "$length\t";
+	$lengthGene += $length;
+
+	# we will count the number of bases in this exon whose coverage $cov is:
+	# $cov >= 20x, 10x <= $cov < 20x, or $cov < 10x respectively
+	my ($basesCovered20,$basesCovered10,$basesCovered0) = (0,0,0);
+
+	# range of interest: start at -10 and end at +10
+	my $range = "$chr:".($starts[$i]-10)."-".($ends[$i]+10);
+	# grab GVCF lines in our range of interest
+	my $tabix = "tabix $gvcf $range";
+
+	open(GVCF, "$tabix |") ||
+	    die "cannot tabix-open the gvcf of interest with:\n$tabix\n";
+	while (my $gvcfLine = <GVCF>) {
+	    chomp($gvcfLine);
+	    my @gvcfFields = split(/\t/,$gvcfLine);
+	    my ($pos,$info,$format,$data) = @gvcfFields[1,7..9];
+	    my $end = $pos;
+	    # if this is a non-var block...
+	    if ($info =~ /END=(\d+);/) {
+		$end = $1;
+	    }
+	    # this line must overlap $range but it may go beyond, adjust if needed
+	    if ($pos < $starts[$i]-10) {
+		#sanity
+		($end >= $starts[$i]-10) ||
+		    die "tabix gave a line for $range that doesn't overlap it (before, end==$end):\n$gvcfLine\n";
+		$pos = $starts[$i]-10;
+	    }
+	    if ($end > $ends[$i]+10) {
+		#sanity
+		($pos <= $ends[$i]+10) ||
+		    die "tabix gave a line for $range that doesn't overlap it (after):\n$gvcfLine\n";
+		$end = $ends[$i]+10;
+	    }
+	    # number of bases overlapping range in this line
+	    my $numBases = $end - $pos + 1;
+
+	    # what's the coverage?
+	    my $coverage = 0;
+	    # %format: key is a FORMAT key (eg MIN_DP), value is the index of that key in $format
+	    my %format;
+	    { 
+		my @format = split(/:/, $format);
+		foreach my $i (0..$#format) {
+		    $format{$format[$i]} = $i ;
+		}
+	    }
+	    my @data = split(/:/,$data);
+	    # if MIN_DP exists, use that
+	    if (defined $format{"MIN_DP"}) {
+		$coverage = $data[$format{"MIN_DP"}];
+	    }
+	    else {
+		# grab the depth (DP or DPI, whichever is defined and higher)
+		if ((defined $format{"DP"}) && ($data[$format{"DP"}]) && ($data[$format{"DP"}] ne '.')) {
+		    $coverage = $data[$format{"DP"}];
+		}
+		if ((defined $format{"DPI"}) && ($data[$format{"DPI"}]) && ($data[$format{"DPI"}] ne '.') &&
+		    ($data[$format{"DPI"}] > $coverage)) {
+		    $coverage = $data[$format{"DPI"}];
+		}
+	    }
+
+	    if ($coverage >= 20) {
+		$basesCovered20 += $numBases;
+	    }
+	    elsif ($coverage >= 10) {
+		$basesCovered10 += $numBases;
+	    }
+	    else {
+		$basesCovered0 += $numBases;
+	    }
+
+	}
+	close(GVCF);
+
+	# we didn't deal with the Strelka call-HR-before-indel bug, and we
+	# didn't take into account the REF size (for DELs or MNPs), so the
+	# result will be approximate... still, make sure we're close enough
+	my $basesTotal = $basesCovered20 + $basesCovered10 + $basesCovered0;
+	if ( (abs($length - $basesTotal) / $length) > 0.1) {
+	    warn "W: more than 10% difference between bases counted ($basesTotal) and length for: $toPrint\n";
+	}
+	my $frac = $basesCovered20 / $basesTotal;
+	$toPrint .= sprintf("%.2f",$frac)."\t";
+	$frac = ($basesCovered20 + $basesCovered10) / $basesTotal;
+	$toPrint .= sprintf("%.2f",$frac)."\n";
+
+	print $toPrint;
+	# done with this exon but also record stats for the gene
+	$bases20Gene += $basesCovered20;
+	$bases10Gene += $basesCovered10;
+	$bases0Gene += $basesCovered0;
+    }
+
+    # done printing data for each exon of this gene, now print global stats for $gene
+    my $toPrint = "$gene\t$transcript\tALL\t$lengthGene\t";
+    my $basesTotalGene = $bases20Gene + $bases10Gene + $bases0Gene;
+    if ( (abs($lengthGene - $basesTotalGene) / $lengthGene) > 0.1) {
+	warn "W: GENE LEVEL, more than 10% difference between bases counted ($basesTotalGene) and length==$lengthGene for: $gene\n";
+    }
+    my $frac = $bases20Gene / $basesTotalGene;
+    $toPrint .= sprintf("%.2f",$frac)."\t";
+    $frac = ($bases20Gene + $bases10Gene) / $basesTotalGene;
+    $toPrint .= sprintf("%.2f",$frac)."\n";
+    print $toPrint;
+}
+
+close(GENES);
+
