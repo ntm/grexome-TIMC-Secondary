c3b61ac50823f75e7735bc694722db6c50f5a073
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2020-09-24 10:39:58 +0200

    Transcripts_Data/ : update to ensembl 101

--- Transcripts_Data/listCanonicalTranscripts_MYSQL.sh
+++ Transcripts_Data/listCanonicalTranscripts_MYSQL.sh
@@ -14,7 +14,7 @@
 # It can't be run at TIMC because it needs access to TCP/3306,
 # which is filtered at TIMC. Just run it from eg nicofree.
 
-DATABASE=homo_sapiens_core_97_38
+DATABASE=homo_sapiens_core_101_38
 
 echo "USE $DATABASE ; select t.stable_id from transcript t, gene g where t.transcript_id = g.canonical_transcript_id ;" | mysql -u anonymous -h ensembldb.ensembl.org | tail -n +2 | sort -u
 
