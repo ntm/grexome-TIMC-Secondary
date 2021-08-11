d784046e45dad1d4effa0c7b342be10f282ae93e
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2021-08-11 16:13:29 +0200

    8_extractSamples.pl trivial

--- 8_extractSamples.pl
+++ 8_extractSamples.pl
@@ -93,7 +93,7 @@ while (my $inFile = readdir(INDIR)) {
     ($inFile =~ /^\./) && next;
     my ($cohort,$fileEnd,$gz);
     if ($inFile =~ (/^([^\.]+)\.(.*csv)$/)) {
-	# $fileEnd allows for .filtered , .pick etc...
+	# $fileEnd allows for .filtered , .canon etc...
 	($cohort,$fileEnd) = ($1,$2);
     }
     elsif ($inFile =~ (/^([^\.]+)\.(.*csv)\.gz$/)) {
