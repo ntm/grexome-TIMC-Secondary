1cf58fc6074562182238f8e6ce63e0a8aa9cd08c
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2022-06-16 13:07:39 +0200

    grexome-TIMC-secondary.pl : recognize deepVariant files for tagging

--- grexome-TIMC-secondary.pl
+++ grexome-TIMC-secondary.pl
@@ -226,8 +226,8 @@ my $numSamples = 0;
 # variant-caller string/name, will be added to all final filenames
 my $caller;
 # try to find the caller name in $inFile
-foreach my $c ("Strelka", "GATK", "ElPrep") {
-    if ($inFile =~ /$c/) {
+foreach my $c ("Strelka", "GATK", "ElPrep", "DeepVariant") {
+    if ($inFile =~ /$c/i) {
 	$caller = $c;
 	warn "I $0: variant-caller id $caller will be appended to all filenames\n";
 	last;
