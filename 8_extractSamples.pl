9604a774ed2a74051cf489bfe2f77a5e742a9a09
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2022-07-18 11:03:56 +0200

    8_extractSamples.pl : trivial

--- 8_extractSamples.pl
+++ 8_extractSamples.pl
@@ -147,7 +147,7 @@ while (my $inFile = readdir(INDIR)) {
 	}
     }
     (($hvCol >= 0) && ($hvColOC >= 0)) || 
-	die "E: $0 - couldn't find {$cohort}_HV or {$cohort}_OTHERCAUSE_HV in header of infile $inFile\n";
+	die "E: $0 - couldn't find ${cohort}_HV or ${cohort}_OTHERCAUSE_HV in header of infile $inFile\n";
     (($hetCol >= 0) && ($hetColOC >= 0)) || 
 	die "E: $0 - couldn't find HET or OC_HET in header of infile $inFile\n";
 
