d63380f2217f12823d5be24100b6b7915006096a
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2022-10-21 11:25:46 +0200

    Transcripts_Data/ update to Ensembl v108, and simplify update process

--- Transcripts_Data/listCanonicalTranscripts_MYSQL.sh
+++ Transcripts_Data/listCanonicalTranscripts_MYSQL.sh
@@ -12,9 +12,11 @@
 # This is functionally equivalent to listCanonicalTranscripts_API.pl
 # but the latter took 16h to run, this takes seconds...
 # It can't be run at TIMC because it needs access to TCP/3306,
-# which is filtered at TIMC. Just run it from eg nicofree.
+# which is filtered at TIMC. You need to run it from a network that
+# doesn't filter outgoing TCP/3306 - I run it from home.
 
-DATABASE=homo_sapiens_core_107_38
+# DATABASE must be set by caller, it should look similar to:
+# DATABASE=homo_sapiens_core_108_38
 
 echo "USE $DATABASE ; select t.stable_id from transcript t, gene g where t.transcript_id = g.canonical_transcript_id ;" | mysql -u anonymous -h ensembldb.ensembl.org | tail -n +2 | sort -u
 
