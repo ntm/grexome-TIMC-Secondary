1a07e672033e3711f1d5f8037888b6508613e679
ntm <Nicolas.Thierry-Mieg@univ-grenoble-alpes.fr>
2026-02-03 18:50:41 +0100

    grexome-TIMC-secondary_runAll.pl: auto-detect latest infiles

--- grexome-TIMC-secondary_runAll.pl
+++ grexome-TIMC-secondary_runAll.pl
@@ -23,20 +23,20 @@
 # 26/08/2021
 # NTM
 
-# Wrapper for running grexome-TIMC-Secondary.pl in parallel on 
-# all variant-caller GVCFs.
-# This script is very much install-specific and can't be re-used as-is,
-# but it serves as a full-blown example since this is how we use the pipeline
-# routinely: when we obtain new samples we simply update $primDate, $secDate and
-# $cnvFile , then run the script.
+# Wrapper for running grexome-TIMC-Secondary.pl in parallel on all variant-caller GVCFs.
+# This script is install-specific and can't be re-used as-is, but it's a great
+# full-blown example since this is how we use grexome-TIMC-Secondary routinely:
+# When new samples are received:
+# -> update GVCFs (with grexome-TIMC-Primary, https://github.com/ntm/grexome-TIMC-Primary )
+# -> update CNV calls (with JACNEx, https://github.com/ntm/JACNEx )
+# -> run this script, it auto-finds the most recent GVCFs ($infile) and CNVs ($cnvFile) and
+#    produces analysis-ready files in a date-stamped ($secDate) outDir.
 
 use strict;
 use warnings;
 
-my $primDate = "250511";
-my $secDate =  "250515";
-
-my $cnvFile = "/home/nthierry/PierreRay_DATA/RunJACNEx/JACNEx_canonical_114/VCFs/CNVs_2025-05-12_15-54-14_NODUPs.vcf.gz";
+my $secDate =  `/bin/date +%y%m%d`;
+chomp($secDate);
 
 # set to "--canonical" to limit to canonical transcripts
 #my $canon = "";
@@ -48,14 +48,22 @@ my $metaPath = "/home/nthierry/GrexomeZoufris/WES/";
 my $binPath = "/home/nthierry/Software/Grexome-TIMC/grexome-TIMC-Secondary/";
 
 foreach my $caller ("Strelka", "GATK") {
+#foreach my $caller ("Strelka") {
+#foreach my $caller ("GATK") {
     my $callerLow = lc($caller);
     my $com = "perl $binPath/grexome-TIMC-secondary.pl --samples=$metaPath/patient_summary.xlsx ";
     $com .= "--pathologies=$metaPath/MetaData/pathologies.xlsx ";
     $com .= "--candidateGenes=$metaPath/MetaData/candidateGenes_allPhenotypes.xlsx ";
-    $com .= "--infile=../GVCFs_grexome/GVCFs_${caller}_Filtered_Merged/grexomes_${callerLow}_merged_$primDate.g.vcf.gz ";
-    ($cnvFile) && ($com .= "--cnvs=$cnvFile ");
+#    $com .= "--candidateGenes=$metaPath/MetaData/PIWI_genes.xlsx ";
+    my $infile = `/bin/ls -1rt ../GVCFs_grexome/GVCFs_${caller}_Filtered_Merged/grexomes_${callerLow}_merged_*.g.vcf.gz | tail -n 1`;
+    chomp($infile);
+    my $cnvFile = `/bin/ls -1rt ../RunJACNEx/JACNEx_canonical_*/VCFs/CNVs_*_NODUPs.vcf.gz | tail -n 1`;
+    chomp($cnvFile);
+    $com .= "--infile=$infile --cnvs=$cnvFile ";
     $com .= "--outDir=SecondaryAnalyses_${secDate}_$caller ";
     $com .= "$canon ";
+    # compare VEP annotations for the same variants from one VEP run to another
+    # $com .= "--debugVep ";
     $com .= "2> grexomeTIMCsec_${secDate}_$caller.log ";
     # move log into caller results subdir
     $com .= "; mv grexomeTIMCsec_${secDate}_$caller.log SecondaryAnalyses_${secDate}_$caller/ ";
